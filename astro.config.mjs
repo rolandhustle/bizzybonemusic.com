28b63b527a68081d02ea0e67e7bd7b1fc395c617
Admin <admin@Admins-MacBook-Pro.local>
2026-04-10 16:37:40 +0200

    Update site URL to bizzybonemusic.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://bizzybonemusic.vercel.app',
+  site: 'https://bizzybonemusic.com',
   integrations: [sitemap()],
   legacy: {
     collectionsBackwardsCompat: true,
